2c2b68a85362d5c42c060ed17013928f2a00e516
Amarnath0510 <amarmuthu007@gmail.com>
2021-12-03 12:49:42 +0530

    changed cors place

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
-
+import cors from 'cors';
 import express, { request, response } from "express";
 import { MongoClient } from 'mongodb';
 import dotenv from "dotenv";
 import { moviesRouter } from "./routes/movies.js";
-import cors from 'cors';
+
  
 dotenv.config();
 console.log(process.env);
@@ -12,7 +11,7 @@ console.log(process.env);
 
 const app=express();
 const PORT=process.env.PORT;
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
  const MONGO_URL=process.env.MONGO_URL;
